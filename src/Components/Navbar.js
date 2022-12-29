a5700d944641cd7184eda02712957263ce26fd4b
[Soumya Ranjan] <soumyaranjan1819@gmail.com>
2022-12-30 03:08:43 +0530

    navbar modified

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className=' flex justify-between bg-slate-500 px-28 py-3 text-white'>
+    <div className=' flex justify-between bg-slate-500 px-12 py-3 text-white'>
         <Link to={'/'} className='text-4xl font-semibold' >TeeRex</Link>
         <Link to={'/cart'} >Cart</Link>
     </div>
